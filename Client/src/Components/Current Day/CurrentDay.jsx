9f3c4d840ed30f7230b8cee81c5a30c4cfc31f78
T_NahuelReboledo <thiagoreboledo8@gmail.com>
2023-09-22 11:30:38 -0300

    disenosB

--- Client/src/Components/Current Day/CurrentDay.jsx
+++ Client/src/Components/Current Day/CurrentDay.jsx
@@ -4,14 +4,16 @@ import style from "./CurrentDay.module.css";
 function CurrentDay({ infoLocation }) {
    return (
       <>
-         <div className={style.image_container}>IMG</div>
+         <div className={style.image_container}>
+
+         </div>
 
          <div className={style.data_current_container}>
-            <div className={style.temp}>{infoLocation.temp}</div>
+            <div className={style.temp}>{infoLocation.temp}°</div>
 
             <div className={style.data}>
-               <h2>
-                  {infoLocation.region}-{infoLocation.country}
+               <h2 className={style.region_country}>
+                  {infoLocation.region} - {infoLocation.country}
                </h2>
                <h3>
                   {infoLocation.time}
@@ -19,8 +21,8 @@ function CurrentDay({ infoLocation }) {
             </div>
 
             <div className={style.condition}>
-               <img src={infoLocation.iconWeather} alt="" />
-               <h4>{infoLocation.condition}</h4>
+               <img src={infoLocation.iconWeather} alt={infoLocation.iconWeather} />
+               <h4 className={style.text_cond}>{infoLocation.condition}</h4>
             </div>
          </div>
       </>
